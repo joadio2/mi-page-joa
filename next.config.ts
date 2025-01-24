51447ce0207a270ae0f8134f782ae8eaf1082ec8
joadio2 <jos674x@gmail.com>
2025-01-24 09:49:22 +0000

    v2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
-import type { Configuration } from "webpack";
+
 const nextConfig = {
-  webpack(config: Configuration) {
+  webpack(config: any) {
     config.module?.rules?.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
